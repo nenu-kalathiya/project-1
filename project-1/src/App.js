9affe949a6e3d4f5f85a5465fac795ba0ca810bc
nenu-kalathiya <nenukalathiya111@gmail.com>
2022-05-31 17:51:24 +0530

    employee-data

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -5,70 +5,17 @@ import Demo from './components/Demo';
 
 function App() {
 
-  let data = [
+  let data = 
     {
       name: "amit",
       age: 35,
       salary: 40000,
       bonus: 1000,
       status: true
-    },
-    {
-      name: "ajay",
-      age: 25,
-      salary: 38000,
-      bonus: 2000,
-      status: false
-    },
-    {
-      name: "mayur",
-      age: 23,
-      salary: 50000,
-      bonus: 500,
-      status: true
-    },
-    {
-      name: "jay",
-      age: 29,
-      salary: 35000,
-      bonus: 600,
-      status: true
-    },
-    {
-      name: "raj",
-      age: 33,
-      salary: 22000,
-      bonus: 2000,
-      status: true
-    },
-  ];
+    }
 
-  return (
-    <table border="1" align="center" cellPadding="10" cellspacing="0">
-      <tr>
-        <td>Name</td>
-        <td>Age</td>
-        <td>Salary</td>
-        <td>Bonus</td>
-        <td>Status</td>
-      </tr>
-      {
-        data.map((d,i) => {
-          let {name, age, salary, bonus, status} = d
-          return(
+  console.log(data.name);
 
-            <tr>
-              <td>{name}</td>
-              <td>{age}</td>
-              <td>{salary}</td>
-              <td>{bonus}</td>
-              <td>{status.toString()}</td>
-            </tr>
-          )
-        }) 
-      }
-    </table>
-  );
 }
 
 export default App;
