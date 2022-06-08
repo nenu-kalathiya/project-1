5e7758c3a48cc8cf7c40014b49e5a33f6cec0618
nenu-kalathiya <nenukalathiya111@gmail.com>
2022-06-08 13:45:48 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './App.css';
+import Counter from './container/Counter';
 import Time from './container/Time/Time';
 
 function App(props) {
   return (
     <div>
-      <Time />
+      {/* <Time /> */}
+      <Counter />
     </div>
   );
 }
